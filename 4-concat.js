67c0033f65590d240cfed5e131307bf48c27ed57
newman-agyei <agyeinewman@gmail.com>
2025-07-10 10:09:31 +0000

    added concat js file

---
+++ 4-concat.js
@@ -0,0 +1,6 @@
+// Get the first two arguments
+const firstArgument = process.argv[2];
+const secondArgument = process.argv[3];
+
+// Print in the format "argument1 is argument2"
+console.log(firstArgument + " is " + secondArgument);

08fcd6cd4744727cabd5c6e7ba3a6d1ac5f5d2ce
newman-agyei <agyeinewman@gmail.com>
2025-07-10 11:22:43 +0000

    added multi-languages loop

---
+++ 6-multi_languages_loop.js
@@ -0,0 +1,7 @@
+// Create an array of strings
+const messages = ["C is fun", "Python is cool", "JavaScript is amazing"];
+
+// Use a for loop to print each message
+for (let i = 0; i < messages.length; i++) {
+  console.log(messages[i]);
+}
